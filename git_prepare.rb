5070b0170214f2cd6355c0d8a883e7900f007c77
John Murray <murrayj5@nku.edu>
2010-06-15 22:03:59 -0400

    added git_prepare

---
+++ git_prepare.rb
@@ -0,0 +1,22 @@
+def prepare_project( _dir )
+    
+    if File.exist? _dir and File.directory? _dir
+    
+        Dir.chdir( _dir )
+        
+        if Dir.entries('.').size - 2 == 0
+            f = File.new('.gitignore', 'w')
+            f.close
+            return nil
+        end
+        
+        Dir.entries('.').each do |item|
+            next if item.to_s == '.' || item.to_s == '..'
+            prepare_project( item ) if File.directory? item
+        end
+    
+    end
+    
+end
+
+def prepare_project( Dir.pwd + '/' + ARGV[0] )
